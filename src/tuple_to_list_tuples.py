f11eb8a5c755e8faa66b39ae58a6ab9f85db25fe
Andres Rodriguez <anrocox@gmail.com>
2014-07-06 17:01:14 -0500

    How to convert a tuple in a list in python?

---
+++ src/tuple_to_list_tuples.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 3, 2014
+
+@author: anroco
+
+How to convert a tuple in a list in python?
+
+¿Como convertir una tupla en una lista en python?
+'''
+
+#create list
+lista = [2, 5, 7, 1, 4, 6]
+print(lista)
+
+#use the tuple() function built-in python, passing as parameter the list
+tupla = tuple(lista)
+print (tupla)
